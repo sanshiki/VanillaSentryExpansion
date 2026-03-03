97e354af900f9152266d3a3a5f029c7bba48eda6
wuzifei <1037890231@qq.com>
2026-03-03 17:13:40 +0800

    adjust damage decay

--- Content/Projectiles/Summon/MachineGunSentryBullet.cs
+++ Content/Projectiles/Summon/MachineGunSentryBullet.cs
@@ -61,7 +61,7 @@ namespace SummonerExpansionMod.Content.Projectiles.Summon
 
         private bool DamageDebug = false;
 
-        private const float DAMAGE_DECAY_FACTOR = 0.8f;
+        private const float DAMAGE_DECAY_FACTOR = 0.6f;
 
         public override void SetDefaults()
         {
